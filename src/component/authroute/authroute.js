849333e496da70deb062d0cf858cb65f8a066dfe
132yse <1533540012@qq.com>
2018-07-30 11:39:01 +0800

    完善localstorage逻辑

--- src/component/authroute/authroute.js
+++ src/component/authroute/authroute.js
@@ -1,18 +1,24 @@
 import React from 'react'
 import axios from 'axios'
 import {withRouter} from 'react-router-dom'
+import {setStorage} from "common/js/localstorage"
+import {getUserInfo} from "api/user"
 
 @withRouter
 class AuthRoute extends React.Component {
 
   componentDidMount() {
-
-    // 是否登录
     axios.get('/auth').then(res => {
         if (res.data.code === 401) {
           this.props.history.push('/login')
         }else {
           this.props.history.push('/')
+          const user = getStorage('user-info')
+          if(!user){
+            getUserInfo(name).then(res => {
+              setStorage('user-info', res.data.user)
+            })
+          }
         }
     })
 
