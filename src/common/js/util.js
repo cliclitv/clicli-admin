136ad5bd068550706d604a346876e81e3e850ba4
132yse <1533540012@qq.com>
2019-05-22 11:13:29 +0800

    日常提交

--- src/common/js/util.js
+++ src/common/js/util.js
@@ -13,7 +13,12 @@ export function replaceContent(content) {
   if (content.indexOf('ftn.qq.com') > -1) content = content.replace('ftn_handler/', 'rkey=')
   if (content.indexOf('ksyun.php') > -1) content = content.replace('/static/danmu/ksyun.php?', '') + '@ksyun'
   if (content.indexOf('zhilian2.php') > -1) content = content.replace('http://119.23.209.33/static/danmu/zhilian2.php?http://47.94.243.190:8081/play/', '') + '@c_360'
-  if (content.indexOf('san.php') > 1) content = content.replace('/static/danmu/san.php?http://193.112.131.234:8081/zzzfun/v360?v=', '') + '@s_360'
+  if (content.indexOf('san.php') > -1) content = content.replace('http://www.zzzfun.com/static/danmu/san.php?v360?v=', '') + '@s_360'
+  if (content.indexOf('sharepoint') > -1) {
+    content = decodeURIComponent(content)
+    content = content.replace(/personal([\s\S]+?)id=\//g, '').replace(/&parent(\S*)/g, '')
+    console.log(content)
+  }
   return content
 }
 
